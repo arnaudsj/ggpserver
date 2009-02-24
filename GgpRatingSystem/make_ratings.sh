fe33540888cb83b079c4b472fccb979a763aa15e
s3882841 <s3882841@6616179e-8559-40e2-96db-df1049cb590e>
2009-02-24 14:43:16 +0000

    - included svg to png conversion
    git-svn-id: https://ggpserver.svn.sourceforge.net/svnroot/ggpserver/trunk@83 6616179e-8559-40e2-96db-df1049cb590e

--- GgpRatingSystem/make_ratings.sh
+++ GgpRatingSystem/make_ratings.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #     Copyright (C) 2008,2009 Martin Günther <mintar@gmx.de>
-# 
+#
 #     This file is part of GgpRatingSystem.
-# 
+#
 #     GgpRatingSystem is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
 #     the Free Software Foundation, either version 3 of the License, or
 #     (at your option) any later version.
-# 
+#
 #     GgpRatingSystem is distributed in the hope that it will be useful,
 #     but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
-# 
+#
 #     You should have received a copy of the GNU General Public License
 #     along with GgpRatingSystem.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -41,10 +41,14 @@ do \
 	mv $YEAR/constant_linear_regression_$LEARNING_RATE.html results$YEAR.html
 done
 
-
-
 for FILE in *.gnuplot
-	do gnuplot < $FILE
+do gnuplot < $FILE
+done
+
+for SVGFILE in *.svg
+do \
+	PNGFILE=$(basename $SVGFILE .svg).png
+	convert $SVGFILE $PNGFILE
 done
 
 rmdir 2005 2006 2007 2008
