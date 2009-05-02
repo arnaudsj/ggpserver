041dd48bb8a077ccd9d75ce1476da532f9f650c9
s3882841 <s3882841@6616179e-8559-40e2-96db-df1049cb590e>
2009-05-02 15:08:26 +0000

    - made dbconnector exchangeable
    git-svn-id: https://ggpserver.svn.sourceforge.net/svnroot/ggpserver/trunk@144 6616179e-8559-40e2-96db-df1049cb590e

--- ggpserver/src/tud/ggpserver/scheduler/RoundRobinScheduler.java
+++ ggpserver/src/tud/ggpserver/scheduler/RoundRobinScheduler.java
@@ -1,14 +1,20 @@
 package tud.ggpserver.scheduler;
 
 import tud.gamecontroller.game.javaprover.Term;
+import tud.ggpserver.datamodel.AbstractDBConnector;
+import tud.ggpserver.datamodel.DBConnectorFactory;
 import cs227b.teamIago.util.GameState;
 
 public class RoundRobinScheduler extends AbstractRoundRobinScheduler<Term, GameState> {
 	private static AbstractRoundRobinScheduler instance = null;
 	
+	private RoundRobinScheduler(AbstractDBConnector dbConnector) {
+		super(dbConnector);
+	}
+
 	public static AbstractRoundRobinScheduler getInstance() {
 		if (instance == null) {
-			instance = new RoundRobinScheduler();
+			instance = new RoundRobinScheduler(DBConnectorFactory.getDBConnector());
 		}
 		return instance;
 	}
