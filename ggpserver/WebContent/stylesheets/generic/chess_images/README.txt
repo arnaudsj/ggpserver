be1d0fa2012d05fbe2b46d74aa16dace1f4a74fd
conroybumpus <conroybumpus@6616179e-8559-40e2-96db-df1049cb590e>
2009-06-04 13:12:44 +0000

    automatically get stylesheets from gamecontroller when building war using my-build.xml
    git-svn-id: https://ggpserver.svn.sourceforge.net/svnroot/ggpserver/trunk@240 6616179e-8559-40e2-96db-df1049cb590e

--- ggpserver/WebContent/stylesheets/generic/chess_images/README.txt
+++
@@ -1,1 +0,0 @@
-all images are from wikimedia: http://commons.wikimedia.org/wiki/Category:Standard_chess_tiles
--- ggpserver/get_stylesheets_from_gamecontroller.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-GameControllerStylesheetsDIR=../gamecontroller/resources/stylesheets/
-GGPServerStylesheetsDIR=WebContent/stylesheets/
-
-( cd ${GameControllerStylesheetsDIR} ; tar --exclude=.svn --exclude=sitespecific.xsl -cf - * ) | (cd ${GGPServerStylesheetsDIR} ; tar -xvf - )
