7860b02d9ad41efce62981b3ee53e43f82d1b290
martin-guenther <martin-guenther@6616179e-8559-40e2-96db-df1049cb590e>
2009-06-06 16:46:44 +0000

    - separated MatchInterface from RunnableMatchInterface
    git-svn-id: https://ggpserver.svn.sourceforge.net/svnroot/ggpserver/trunk@254 6616179e-8559-40e2-96db-df1049cb590e

---
+++ gamecontroller/java/tud/gamecontroller/game/MatchInterface.java
@@ -0,0 +1,46 @@
+/*
+    Copyright (C) 2008 Stephan Schiffel <stephan.schiffel@gmx.de>
+
+    This file is part of GameController.
+
+    GameController is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GameController is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GameController.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package tud.gamecontroller.game;
+
+/**
+ * The point of this interfaces is not to refer to the actual Player
+ * implementation, only to PlayerInfos. This makes Matches that are not intended
+ * to be run (e.g., finished or aborted matches) much more lightweight and
+ * removes actual implementation details from these matches, because they do not
+ * even need to know about reasoners.<br>
+ * 
+ * If you need a match that provides Player implementations instead of only
+ * PlayerInfos, use RunnableMatchInterface.
+ */
+public interface MatchInterface<TermType, StateType extends StateInterface<TermType, ? extends StateType>> {
+
+	public abstract String getMatchID();
+
+	public abstract GameInterface<TermType, StateType> getGame();
+
+	public abstract int getStartclock();
+
+	public abstract int getPlayclock();
+
+	// TODO: This could be extended to include the following methods:
+	//	public abstract Collection<? extends PlayerInfo<TermType>> getPlayerInfos();
+	//	public abstract List<? extends PlayerInfo> getOrderedPlayerInfos();
+	//	public abstract PlayerInfo getPlayerInfo(RoleInterface<TermType> role);
+}
