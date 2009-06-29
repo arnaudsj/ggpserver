5ba8dad5f3d2ebee2df090b371fe097e3d45a499
martin-guenther <martin-guenther@6616179e-8559-40e2-96db-df1049cb590e>
2009-06-29 16:57:34 +0000

    - added JUnit tests
    git-svn-id: https://ggpserver.svn.sourceforge.net/svnroot/ggpserver/trunk@283 6616179e-8559-40e2-96db-df1049cb590e

---
+++ gamecontroller/tests/tud/gamecontroller/traces/MatchTraceBuilderTest.java
@@ -0,0 +1,76 @@
+/*
+    Copyright (C) 2009 Martin Günther <mintar@gmx.de>
+
+    This file is part of GameController.
+
+    GameController is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GameController is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GameController.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+package tud.gamecontroller.traces;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+import static org.junit.Assert.*;
+import static tud.gamecontroller.traces.MatchTraceReaderWriterTest.*;
+
+public class MatchTraceBuilderTest {
+
+    public MatchTraceBuilderTest() {
+    }
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+	
+	@Test
+	public void testBuilder() throws InterruptedException, IOException, TransformerFactoryConfigurationError, TransformerFactoryConfigurationError, TransformerException, TransformerException, ParserConfigurationException, SAXException {
+		File inputDir = new File("testdata/traces/");
+		File gamesDir = new File("testdata/games/");
+		
+		for (File inputFile : inputDir.listFiles()) {
+			String gameName = inputFile.getName().substring(0, inputFile.getName().indexOf("."));
+			
+			File gameFile = new File(gamesDir, gameName + ".lisp");
+			File outputFile = File.createTempFile(gameName, ".trace.xml");
+			
+			RetraceGameControllerRunner.retrace(inputFile, outputFile, gameFile);
+			
+			assertTrue("retrace did not produce identical output: " + inputFile, equalFiles(inputFile, outputFile));
+			outputFile.delete();
+		}
+	}
+
+}
---
+++ gamecontroller/tests/tud/gamecontroller/traces/MatchTraceReaderWriterTest.java
@@ -0,0 +1,91 @@
+/*
+    Copyright (C) 2009 Martin Günther <mintar@gmx.de>
+
+    This file is part of GameController.
+
+    GameController is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GameController is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GameController.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+package tud.gamecontroller.traces;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class MatchTraceReaderWriterTest {
+
+    public MatchTraceReaderWriterTest() {
+    }
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+	@Test
+	public void testReadWrite() throws Exception {
+		File datadir = new File("testdata/traces/");
+		
+		for (File inputFile : datadir.listFiles()) {
+			MatchTraceReader reader = new MatchTraceReader();
+			MatchTrace trace = reader.read(inputFile);
+			
+			File outputFile = File.createTempFile("trace", ".xml");
+			
+			MatchTraceWriter writer = new MatchTraceWriter();
+			writer.write(trace, outputFile);
+			
+			assertTrue("read/write did not produce identical output: " + inputFile, equalFiles(inputFile, outputFile));
+			outputFile.delete();
+		}
+	}
+	
+	public static boolean equalFiles(File file1, File file2) throws IOException {
+		LineNumberReader reader1 = new LineNumberReader(new FileReader(file1));
+		LineNumberReader reader2 = new LineNumberReader(new FileReader(file2));
+		
+		String line1, line2;
+		
+		while (true) {				
+			line1 = reader1.readLine();
+			line2 = reader2.readLine();
+			
+			if (line1 == null && line2 == null) {
+				return true;
+			} else if (line1 == null || !line1.equals(line2)) {
+				return false;
+			}
+		}
+	}
+}
