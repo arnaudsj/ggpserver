f970fda8f7048e5902c5a37074bd319daa9226bf
conroybumpus <conroybumpus@6616179e-8559-40e2-96db-df1049cb590e>
2010-06-23 08:51:36 +0000

    git-svn-id: https://ggpserver.svn.sourceforge.net/svnroot/ggpserver/trunk@457 6616179e-8559-40e2-96db-df1049cb590e

--- ggpserver/01_create_database.sql
+++ ggpserver/01_create_database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 22, 2010 at 02:40 PM
+-- Generation Time: Jun 23, 2010 at 10:42 AM
 -- Server version: 5.0.51
 -- PHP Version: 5.2.4-2ubuntu5.10
 
@@ -24,8 +24,8 @@ USE `ggpserver` ;
 --
 
 CREATE TABLE IF NOT EXISTS `config` (
-  `key` varchar(255) character set latin1 collate latin1_general_cs NOT NULL,
-  `value` varchar(255) character set latin1 collate latin1_general_cs default NULL,
+  `key` varchar(255) NOT NULL,
+  `value` varchar(255) default NULL,
   PRIMARY KEY  (`key`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -37,13 +37,13 @@ CREATE TABLE IF NOT EXISTS `config` (
 
 CREATE TABLE IF NOT EXISTS `errormessages` (
   `key` int(11) NOT NULL auto_increment,
-  `match_id` varchar(40) character set latin1 collate latin1_general_cs NOT NULL,
+  `match_id` varchar(40) NOT NULL,
   `step_number` int(11) NOT NULL,
-  `type` varchar(40) character set latin1 collate latin1_general_cs NOT NULL,
-  `message` varchar(255) character set latin1 collate latin1_general_cs NOT NULL,
-  `player` varchar(20) character set latin1 collate latin1_general_cs default NULL,
+  `type` varchar(40) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `player` varchar(20) default NULL,
   PRIMARY KEY  (`key`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=359449 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7793 ;
 
 -- --------------------------------------------------------
 
@@ -52,12 +52,12 @@ CREATE TABLE IF NOT EXISTS `errormessages` (
 --
 
 CREATE TABLE IF NOT EXISTS `games` (
-  `name` varchar(40) character set latin1 collate latin1_general_cs NOT NULL,
-  `gamedescription` longtext character set latin1 collate latin1_general_cs NOT NULL,
-  `stylesheet` varchar(255) character set latin1 collate latin1_general_cs NOT NULL default '../stylesheets/generic/generic.xsl',
+  `name` varchar(40) NOT NULL,
+  `gamedescription` longtext NOT NULL,
+  `stylesheet` varchar(255) NOT NULL default '../stylesheets/generic/generic.xsl',
   `xml_view` longtext character set latin1 collate latin1_general_cs,
   `enabled` tinyint(1) NOT NULL default '0',
-  `creator` varchar(20) character set latin1 collate latin1_general_cs NOT NULL default 'admin',
+  `creator` varchar(20) NOT NULL default 'admin',
   `gdl_version` int(11) NOT NULL default '1',
   PRIMARY KEY  (`name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -69,17 +69,18 @@ CREATE TABLE IF NOT EXISTS `games` (
 --
 
 CREATE TABLE IF NOT EXISTS `matches` (
-  `match_id` varchar(40) character set latin1 collate latin1_general_cs NOT NULL,
-  `game` varchar(40) character set latin1 collate latin1_general_cs NOT NULL,
+  `match_id` varchar(40) NOT NULL,
+  `game` varchar(40) NOT NULL,
   `start_clock` int(11) NOT NULL,
   `play_clock` int(11) NOT NULL,
   `start_time` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  `status` varchar(20) character set latin1 collate latin1_general_cs NOT NULL default 'new',
-  `tournament_id` varchar(40) character set latin1 collate latin1_general_cs NOT NULL,
+  `status` varchar(20) NOT NULL default 'new',
+  `tournament_id` varchar(40) NOT NULL,
   `scrambled` tinyint(1) NOT NULL default '0',
   `weight` double NOT NULL default '1',
-  `owner` varchar(20) character set latin1 collate latin1_general_cs NOT NULL default 'admin',
-  PRIMARY KEY  (`match_id`)
+  `owner` varchar(20) NOT NULL default 'admin',
+  PRIMARY KEY  (`match_id`),
+  KEY `tournament_id` (`tournament_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -89,8 +90,8 @@ CREATE TABLE IF NOT EXISTS `matches` (
 --
 
 CREATE TABLE IF NOT EXISTS `match_players` (
-  `match_id` varchar(40) character set latin1 collate latin1_general_cs NOT NULL,
-  `player` varchar(20) character set latin1 collate latin1_general_cs NOT NULL,
+  `match_id` varchar(40) NOT NULL,
+  `player` varchar(20) NOT NULL,
   `roleindex` smallint(6) NOT NULL,
   `goal_value` tinyint(11) default NULL,
   PRIMARY KEY  (`match_id`,`roleindex`)
@@ -103,10 +104,10 @@ CREATE TABLE IF NOT EXISTS `match_players` (
 --
 
 CREATE TABLE IF NOT EXISTS `moves` (
-  `match_id` varchar(40) character set latin1 collate latin1_general_cs NOT NULL,
+  `match_id` varchar(40) NOT NULL,
   `step_number` int(11) NOT NULL,
   `roleindex` int(11) NOT NULL,
-  `move` text character set latin1 collate latin1_general_cs NOT NULL,
+  `move` text NOT NULL,
   PRIMARY KEY  (`match_id`,`step_number`,`roleindex`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -117,11 +118,11 @@ CREATE TABLE IF NOT EXISTS `moves` (
 --
 
 CREATE TABLE IF NOT EXISTS `players` (
-  `name` varchar(20) character set latin1 collate latin1_general_cs NOT NULL,
-  `host` varchar(255) character set latin1 collate latin1_general_cs NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `host` varchar(255) NOT NULL,
   `port` smallint(6) unsigned NOT NULL,
-  `owner` varchar(20) character set latin1 collate latin1_general_cs NOT NULL,
-  `status` varchar(20) character set latin1 collate latin1_general_cs NOT NULL,
+  `owner` varchar(20) NOT NULL,
+  `status` varchar(20) NOT NULL,
   `plays_round_robin` tinyint(1) NOT NULL default '1',
   `plays_manual` tinyint(1) NOT NULL default '1',
   `gdl_version` int(11) NOT NULL default '1',
@@ -135,10 +136,10 @@ CREATE TABLE IF NOT EXISTS `players` (
 --
 
 CREATE TABLE IF NOT EXISTS `states` (
-  `match_id` varchar(40) character set latin1 collate latin1_general_cs NOT NULL,
+  `match_id` varchar(40) NOT NULL,
   `step_number` int(11) NOT NULL,
-  `state` text character set latin1 collate latin1_general_cs NOT NULL,
-  `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `state` text NOT NULL,
+  `timestamp` timestamp NOT NULL default '0000-00-00 00:00:00',
   PRIMARY KEY  (`match_id`,`step_number`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -149,17 +150,10 @@ CREATE TABLE IF NOT EXISTS `states` (
 --
 
 CREATE TABLE IF NOT EXISTS `tournaments` (
-  `tournament_id` varchar(40) character set latin1 collate latin1_general_cs NOT NULL,
-  `owner` varchar(20) character set latin1 collate latin1_general_cs NOT NULL,
+  `tournament_id` varchar(40) NOT NULL,
+  `owner` varchar(20) NOT NULL,
   PRIMARY KEY  (`tournament_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tournaments`
---
-
-INSERT INTO `tournaments` (`tournament_id`, `owner`) VALUES
-('round_robin_tournament', 'admin');
 
 -- --------------------------------------------------------
 
@@ -168,8 +162,8 @@ INSERT INTO `tournaments` (`tournament_id`, `owner`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_name` varchar(20) character set latin1 collate latin1_general_cs NOT NULL,
-  `user_pass` varchar(40) character set latin1 collate latin1_general_ci NOT NULL,
+  `user_name` varchar(20) NOT NULL,
+  `user_pass` varchar(40) NOT NULL,
   `email_address` varchar(320) character set latin1 collate latin1_general_cs default NULL,
   PRIMARY KEY  (`user_name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -188,10 +182,11 @@ INSERT INTO `users` (`user_name`, `user_pass`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `user_roles` (
-  `user_name` varchar(20) character set latin1 collate latin1_general_cs NOT NULL,
-  `role_name` varchar(20) character set latin1 collate latin1_general_cs NOT NULL,
+  `user_name` varchar(20) NOT NULL,
+  `role_name` varchar(20) NOT NULL,
   PRIMARY KEY  (`user_name`,`role_name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
 
 --
 -- Dumping data for table `user_roles`
